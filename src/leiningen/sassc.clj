a36df0a163bfec851068f2faec2b94dddeb6cad8
kei <apribase@gmail.com>
2014-07-06 11:31:19 +0900

    print once and clean.

--- src/leiningen/sassc.clj
+++ src/leiningen/sassc.clj
@@ -1,4 +1,33 @@
-(ns leiningen.sassc)
+(ns leiningen.sassc
+  "Compile SASS/SCSS source into a CSS file."
+  (:require [leiningen.help :as lhelp]
+            [leiningen.core.main :as lmain]))
 
-(defn sassc [project & args]
-  (println "Hello sassc"))
+
+(defn- once
+  "Compile the SASS/SCSS project once."
+  [project]
+  (println "once"))
+
+
+(defn- clean
+  "Remove automatically generated files."
+  [project]
+  (println "clean"))
+
+
+(defn sassc
+  "Run the sassc plugin."
+  {:help-arglists '([once clean])
+   :subtasks [#'once #'clean]}
+  ([project]
+     (println (lhelp/help-for "sassc"))
+     (lmain/abort))
+  ([project subtask & args]
+     (case subtask
+       "once" (once project)
+       "clean" (clean project)
+       (do (println "Subtask" (str \" subtask \") "not found."
+                    (lhelp/subtask-help-for *ns* #'sassc))
+           (lmain/abort)))))
+
