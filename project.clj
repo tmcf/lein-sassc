060d1b93039c95dec7c0968a64e5eea2cd3815cd
kei <apribase@gmail.com>
2014-07-07 17:09:20 +0900

    Success to build scss files.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject lein-sassc "1.0.0"
-  :description "Leiningen sasscs plugin"
+(defproject lein-sassc "0.9.0"
+  :description "Leiningen sassc plugin"
   :url "http://bitbucket.org/apribase/lein-sassc"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-
-  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.raynes/fs "1.4.6"]]
+  :sassc [{}]
   :eval-in-leiningen true
   :min-lein-version "2.0.0")
