b02d0b51eb5ec1f08c95b8adf4f5f2267d7e29e6
kei <apribase@gmail.com>
2014-07-07 19:50:19 +0900

    add scm for promotion.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject lein-sassc "0.9.0"
+(defproject apribase/lein-sassc "0.9.2"
   :description "Leiningen plugin to compile SASS/SCSS files with SassC."
   :url "http://bitbucket.org/apribase/lein-sassc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+  :scm {:name "hg"
+        :url "https://apribase@bitbucket.org/apribase/lein-sassc"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.6"]]
   :hooks [leiningen.sassc]
