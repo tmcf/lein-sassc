35857d6093eeddee87155610fa8dd3d98b059831
kei <apribase@gmail.com>
2014-12-27 06:28:02 +0900

    fix missing scm

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject lein-sassc "0.10.2"
+(defproject lein-sassc "0.10.3"
   :description "Leiningen plugin to compile SASS/SCSS files with SassC."
   :url "http://bitbucket.org/apribase/lein-sassc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:url "git@github.com:apribase/lein-sassc.git"}
+  :scm {:name "git"
+        :url "git@github.com:apribase/lein-sassc.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.6"]]
   :hooks [leiningen.sassc]
