a0f6f6d66a38bb2ceeafeaab36622d2dd0ef5069
kei <apribase@gmail.com>
2014-12-27 06:39:30 +0900

    fix scm url for clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-sassc "0.10.3"
+(defproject lein-sassc "0.10.4"
   :description "Leiningen plugin to compile SASS/SCSS files with SassC."
   :url "http://bitbucket.org/apribase/lein-sassc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "git@github.com:apribase/lein-sassc.git"}
+        :url "https://github.com/apribase/lein-sassc.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.6"]]
   :hooks [leiningen.sassc]
